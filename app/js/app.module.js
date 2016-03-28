c3119f16202b85fb93b436defff43554dbfb2f34
Alexander Ciesielski <aciesielski@live.com>
2016-03-28 19:30:31 +0200

    Translation for PL finished, GB and DE TODO

--- app/js/app.module.js
+++ app/js/app.module.js
@@ -34,6 +34,17 @@
             CV_1: 'Urodziłem się w 1990 r. w Niemczech. Dwanaście lat później przeprowadziłem się z rodziną do Warszawy, gdzie zdałem maturę  Niemiecko-Polskiej Szkole im. Willyego Brandta.',
             CV_2: 'Następnie skończylem studia inżynierskie na <strong>Polsko-Japońskiej Wyższej Szkole Technik Komputerowych</strong> ze specjalizacją \'<i>Mobilne Urządzenia<i>\'.',
             PROJECTS: 'Historia Projektów',
+            WROCLAW: 'Wrocław',
+            WARSZAWA: 'Warszawa',
+            CAPGEMINI_DESCRIPTION: '<strong>Software Engineer</strong><br />Utrzymywanie kodu / naprawa bugów<br />Administracja bazy danych<br />3rd level support',
+            PENTEGY_DESCRIPTION: '<strong>Junior Developer</strong><br /> Praca nad wielowątkowym modułem dla <a target="_blank" href=\"http://www.mz.gov.pl/system-ochrony-zdrowia/panstwowe-ratownictwo-medyczne/organizacja-systemu-panstwowe-ratownictwo-medyczne/system-wspomagania-dowodzenia-prm\">Systemu Wspomagania Dowodzenia dla Państwowego Ratownictwa Medycznego</a>, którego celem jest przetwarzanie i zapisywanie pakietów otrzymywanych z karetek, zawierających informacje o położeniu, prędkości, włączonych światłach etc.',
+            ACI_DESCRIPTION: '<strong>Analityk IT</strong><br />Przygotowywanie dokumentów projektowych oraz specyfikacji technicznych',
+            BILANS: 'Bilans (Kancelaria podatkowa), Warszawa',
+            BILANS_DESCRIPTION: '<strong>Administrator IT / Web Developer</strong><ul><li><a target="_blank" href=\"http://bilans.eu\">www.bilans.eu</a></li><li><a target="_blank" href=\"http://wszystkodlafirmy.pl\">www.wszystkodlafirmy.pl</a></li><li><a target="_blank" href=\"http://gbadwokaci.pl\">www.gbadwokaci.pl</a></li></ul>',
+            PJATK: 'Polsko-Japonśka Akademia Technik Komputerowych, Warszawa',
+            WBS: 'Polsko-Niemiecka Szkoła Spotkań i Dialogu im. Willy\'ego Brandta, Warszawa',
+            IT_ENGINEER: 'Inżynier Informatyki<br/>(specjalizacja <i>Mobilne Urządzenia</i>)',
+            MATURA: 'Matura polsko-niemiecka',
             EDUCATION: 'Historia Edukacji',
             WHEN: 'Kiedy',
             WHERE: 'Gdzie',
@@ -47,57 +58,23 @@
             SEND: 'Wyślij',
             MESSAGE_SENT: 'Wiadomość została wysłana!',
             ERROR: 'Wystąpił błąd!<br/>Proszę wysłać wiadomość na adres \'alexander@ciesielski.co\'.',
-            
             NAME_TOO_SHORT: 'Wpisane imię i nazwisko są za krótkie.',
             NAME_TOO_LONG: 'Wpisane imię i nazwisko są za długie.',
             NAME_REQUIRED: 'Proszę wpisać imię i nazwisko.',
-            
             EMAIL_REQUIRED: 'Proszę wpisać adres mailowy.',
             EMAIL_INVALID: 'Podany adres mailowy jest nieprawidłowy.',
-            
             MESSAGE_TOO_SHORT: 'Wpisana wiadomość jest za krótka.',
             MESSAGE_TOO_LONG: 'Wpisana wiadomość jest za długa.',
             MESSAGE_REQUIRED: 'Wiadomość nie może być pusta.',
         });
 
         $translateProvider.translations('gb', {
-            NAVIGATION: 'Navigation',
-            TECHNOLOGIES: 'Technologies',
-            CONTACT: 'Contact',
-            HI_MYNAMEIS: 'My name is',
-            ABOUTME: 'I am a full-stack developer interested in all the newest technologies.',
-            ABOUTME_2: 'I specialize in mobile, web and hybrid apps.',
-            ABOUTME_3: 'Let\'s create the app of tomorrow. (cheesy, I know)',
-            LEARN_MORE: 'Learn more',
-            DATABASES: 'Databases',
-            WILL_TO_LEARN: 'Will to learn',
-            NONSTOP_SELFDEVELOPMENT: 'Non-stop self-development',
-            AND_INTEREST_IN_TECHNOLOGIES: 'and interest in newest technologies',
-            CV_1: 'Urodziłem się w 1990 r. w Niemczech. Dwanaście lat później przeprowadziłem się z rodziną do Warszawy, gdzie zdałem maturę  Niemiecko-Polskiej Szkole im. Willyego Brandta.',
-            CV_2: 'Następnie skończylem studia inżynierskie na <strong>Polsko-Japońskiej Wyższej Szkole Technik Komputerowych</strong> ze specjalizacją \'<i>Mobilne Urządzenia<i>\'.',
-            PROJECTS: 'Projects',
-            EDUCATION: 'Education',
-            WHEN: 'When',
-            WHERE: 'Where',
-            WHAT: 'What',
-            IT_ANALYST: 'IT Analyst',
-            READY_TO_HIRE_ME: 'Are you ready to hire me for your next project? Awesome!',
-            USE_THE_CONTACT_FORM: 'Use the contact form below and I will reply ASAP.',
-            NAME_SURNAME: 'Name & Surname',
-            EMAIL: 'Email',
-            MESSAGE: 'Message',
-            SEND: 'Send',
-            MESSAGE_SENT: 'Message sent!',
-            ERROR: 'There was an error sending your message, try my email address.',
 
         });
 
         // englische Sprache
         $translateProvider.translations('de', {
-            APP_HEADLINE: 'Awesome AngularJS App',
-            TECHNOLOGIES: 'Start',
-            NAV_ABOUT: 'About',
-            APP_TEXT: 'Some text about the awesome AngularJS app.'
+                
         });
 
         $translateProvider.preferredLanguage('pl');
