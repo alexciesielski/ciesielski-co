9e8eb16425173c3bd916c78132e253db36573f2b
Alexander Ciesielski <aciesielski@live.com>
2016-03-30 11:21:33 +0200

    Created grunt copy task to /dist/ folder

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,6 +9,42 @@
 	  grunt.initConfig({
 
 		pkg: grunt.file.readJSON('package.json'),
+		
+		copy: {
+		  main: {
+			files: [
+			// copy img folder
+			{
+				expand: true,
+				cwd: 'app/img/',
+				src: '**',
+				dest: 'app/dist/img/',
+			},
+			// copy i18n folder
+			{
+				expand: true,
+				cwd: 'app/i18n/',
+				src: '**',
+				dest: 'app/dist/i18n/',
+			},
+			// copy fonts folder
+			{
+				expand: true,
+				cwd: 'app/fonts/',
+				src: '**',
+				dest: 'app/dist/fonts/',
+			}]
+		  }
+		},
+		
+		less: {
+		  build: {
+			files: {
+			  //'app/dist/css/<%= pkg.name %>.css': 'app/less/app.less'
+			  'app/css/<%= pkg.name %>.css': 'app/less/<%= pkg.name %>.less'
+			}
+		  }
+		},
 
 		jshint: {
 		  /*options: {
@@ -29,22 +65,13 @@
 			  'app/dist/js/<%= pkg.name %>.min.js': 'app/js/app*.js'
 			}
 		  }
-		},
-		
-		less: {
-		  build: {
-			files: {
-			  //'app/dist/css/<%= pkg.name %>.css': 'app/less/app.less'
-			  'app/css/<%= pkg.name %>.css': 'app/less/<%= pkg.name %>.less'
-			}
-		  }
-		},
+		},		
 		
 		cssmin: {
 		  build: {
 			files: {
-			  //'app/dist/css/<%= pkg.name %>.min.css': 'app/dist/css/<%= pkg.name %>.css'
-			  'app/css/<%= pkg.name %>.min.css': ['app/css/ciesielski-co.bootstrap.min.css' , 'app/css/flag-icon.min.css', 'app/css/font-awesome.min.css', 'app/css/animate.min.css', 'app/css/Merriweather400,300,300italic,400italic,700,700italic,900,900italic.css', 'app/css/OpenSans300italic,400italic,600italic,700italic,800italic,400,300,600,700,800.txt', 'app/css/<%= pkg.name %>.css']
+			  //'app/dist/css/<%= pkg.name %>.min.css': 'app/css/<%= pkg.name %>.css'
+			  'app/dist/css/<%= pkg.name %>.min.css': ['app/css/ciesielski-co.bootstrap.min.css' , 'app/css/flag-icon.min.css', 'app/css/font-awesome.min.css', 'app/css/animate.min.css', 'app/css/Merriweather400,300,300italic,400italic,700,700italic,900,900italic.css', 'app/css/OpenSans300italic,400italic,600italic,700italic,800italic,400,300,600,700,800.css', 'app/css/<%= pkg.name %>.css']
 			}
 		  }
 		},
@@ -75,7 +102,7 @@
 
 
 	  // Grunt default task  
-	  grunt.registerTask('default', ['jshint', 'uglify', 'less', 'cssmin']);
+	  grunt.registerTask('default', ['less', 'jshint', 'uglify', 'cssmin']);
 	  
 	};
 }());
