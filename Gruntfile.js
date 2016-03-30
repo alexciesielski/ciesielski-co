efc496b9caa2a35162d0776f18bdd1f2049975f4
Alexander Ciesielski <aciesielski@live.com>
2016-03-30 10:56:47 +0200

    Customized Bootstrap plugin 37kb -> 7kb

--- Gruntfile.js
+++ Gruntfile.js
@@ -26,7 +26,7 @@
 			files: {
 			  //'app/dist/js/<%= pkg.name %>.min.js': ['app/js/app.module.js', 'app/js/app.controller.js', 'app/js/creative.js', 'app/js/typed.min.js']
 			  //'app/dist/js/<%= pkg.name %>.min.js': 'app/js/*.js'
-			  'app/dist/js/uglified-angular-libs.min.js': 'app/js/angu*.js'
+			  'app/dist/js/<%= pkg.name %>.min.js': 'app/js/app*.js'
 			}
 		  }
 		},
@@ -34,22 +34,32 @@
 		less: {
 		  build: {
 			files: {
-			  'app/dist/css/<%= pkg.name %>.css': 'app/less/app.less'
-			  //'app/css/creative.css': 'app/less/app.less'
+			  //'app/dist/css/<%= pkg.name %>.css': 'app/less/app.less'
+			  'app/css/<%= pkg.name %>.css': 'app/less/<%= pkg.name %>.less'
 			}
 		  }
 		},
 		
 		cssmin: {
-		  options: {
-			banner: '/*\n <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> \n*/\n'
-		  },
 		  build: {
 			files: {
-			  'app/dist/css/<%= pkg.name %>.min.css': 'app/dist/css/<%= pkg.name %>.css'
+			  //'app/dist/css/<%= pkg.name %>.min.css': 'app/dist/css/<%= pkg.name %>.css'
+			  'app/css/<%= pkg.name %>.min.css': ['app/css/ciesielski-co.bootstrap.min.css' , 'app/css/flag-icon.min.css', 'app/css/font-awesome.min.css', 'app/css/animate.min.css', 'app/css/Merriweather400,300,300italic,400italic,700,700italic,900,900italic.css', 'app/css/OpenSans300italic,400italic,600italic,700italic,800italic,400,300,600,700,800.txt', 'app/css/<%= pkg.name %>.css']
 			}
 		  }
-		}
+		},
+		
+		concat: {
+			options: {
+			  separator: ';',
+			},
+			dist: {
+			  src: ['app/js/jquery.js', 'app/js/bootstrap.min.js', 'app/js/angular.min.js', 'app/js/firebase.js', 'app/js/angularfire.min.js'],
+			  dest: 'app/dist/js/jquery-bootstrap-angular-firebase-angularfire.js',
+			},
+		  },
+		
+		
 
 	  });
 
@@ -59,6 +69,10 @@
 	  grunt.loadNpmTasks('grunt-contrib-copy');
 	  grunt.loadNpmTasks('grunt-contrib-cssmin');
 	  grunt.loadNpmTasks('grunt-contrib-watch');
+	  grunt.loadNpmTasks('grunt-contrib-concat');
+	  grunt.loadNpmTasks('grunt-contrib-compress');
+	  grunt.loadNpmTasks('grunt-html-build');
+
 
 	  // Grunt default task  
 	  grunt.registerTask('default', ['jshint', 'uglify', 'less', 'cssmin']);
